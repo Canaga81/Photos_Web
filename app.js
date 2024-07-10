b7f8acdd1d1d47b584876ca38af9b648b19e1e9f
Canaga81 <Canagamemmedov@gmail.com>
2024-07-10 11:30:43 +0400

    followers & followings finish

--- app.js
+++ app.js
@@ -1,41 +1,51 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 const PORT = process.env.PORT || 8080;
-const conn = require('./db.js');
-const cookieParser = require('cookie-parser')
-const pageRoute = require('./routes/pageRoute.js');
-const photoRoute = require('./routes/photoRoute.js');
-const userRoute = require('./routes/userRoute.js');
-const { checkUser } = require('./middlewares/authMiddleware.js');
-const fileUpload = require('express-fileupload');
-const cloudinary = require('cloudinary').v2;
+const conn = require("./db.js");
+const cookieParser = require("cookie-parser");
+const methodOverride = require("method-override");
+const pageRoute = require("./routes/pageRoute.js");
+const photoRoute = require("./routes/photoRoute.js");
+const userRoute = require("./routes/userRoute.js");
+const { checkUser } = require("./middlewares/authMiddleware.js");
+const fileUpload = require("express-fileupload");
+const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API,
-    api_secret: process.env.CLOUD_API_SECRET
-})
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
 conn();
 
+
 //^ ejs template engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+
 
 //^ static files middleware
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(fileUpload({useTempFiles: true}))
+app.use(fileUpload({ useTempFiles: true }));
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"],
+  })
+);
+
 
 //^ routes
-app.use('*', checkUser);
-app.use('/', pageRoute);
-app.use('/photos', photoRoute);
-app.use('/users', userRoute);
+app.use("*", checkUser);
+app.use("/", pageRoute);
+app.use("/photos", photoRoute);
+app.use("/users", userRoute);
+
 
 app.listen(PORT, () => {
-    console.log(`Is running on port: ${PORT}`);
+  console.log(`Is running on port: ${PORT}`);
 });
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,181 +1,162 @@
-const Users = require('../models/userModel.js');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const Photos = require('../models/productModel.js');
+const Users = require("../models/userModel.js");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const Photos = require("../models/productModel.js");
 
 const createUser = async (req, res) => {
+  try {
+    const user = await Users.create(req.body);
+    res.status(201).json({ user: user._id });
+  } catch (error) {
+    console.log("ERROR:::", error);
 
-    try {
-        const user = await Users.create(req.body);
-        res.status(201).json({user: user._id})
-    } catch (error) {
+    let errors2 = {};
 
-        console.log("ERROR:::", error);
-
-        let errors2 = {};
-
-        if(error.code === 11000) {
-            errors2.email = "The email is already registered."
-        }
-
-        if(error.name === "ValidationError") {
-            Object.keys(error.errors).forEach((key) => {
-                errors2[key] = error.errors[key].message
-            })
-        }
-
-        console.log("ERRORS", errors2);
-
-        res.status(400).json(errors2)
+    if (error.code === 11000) {
+      errors2.email = "The email is already registered.";
     }
 
-}
+    if (error.name === "ValidationError") {
+      Object.keys(error.errors).forEach((key) => {
+        errors2[key] = error.errors[key].message;
+      });
+    }
+
+    console.log("ERRORS", errors2);
+
+    res.status(400).json(errors2);
+  }
+};
 
 const loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await Users.findOne({ username });
 
-    try {
+    let same = false;
 
-        const { username, password } = req.body;
-        const user = await Users.findOne({username});
-
-        let same = false;
-
-        if(user) {
-            same = await bcrypt.compare(password, user.password);
-        }
-        else {
-            return res.status(401).json({message: "There is no such user !"});
-        }
-
-        if(same) {
-
-            const token = createToken(user._id)
-            res.cookie('jwt', token, {
-                httpOnly: true,
-                maxAge: 1000*60*60*24
-            })
-
-            res.redirect('/users/dashboard');
-
-        }
-        else {
-            res.status(401).json({message: "Passwords are not matched !"});
-        }
-
-    } catch (error) {
-        res.status(500).json({message: error.message});
+    if (user) {
+      same = await bcrypt.compare(password, user.password);
+    } else {
+      return res.status(401).json({ message: "There is no such user !" });
     }
 
-}
+    if (same) {
+      const token = createToken(user._id);
+      res.cookie("jwt", token, {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24,
+      });
+
+      res.redirect("/users/dashboard");
+    } else {
+      res.status(401).json({ message: "Passwords are not matched !" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 const createToken = (userId) => {
-    return jwt.sign({userId}, process.env.JWT_SECRET, {
-        expiresIn: '1d'
-    })
-}
+  return jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+};
 
 const getDashboardPage = async (req, res) => {
+  const photos = await Photos.find({ user: res.locals.user._id });
+  const user = await Users.findById({ _id: res.locals.user._id }).populate([
+    "followers",
+    "followings",
+  ]);
 
-    const photos = await Photos.find({user: res.locals.user._id});
-    const user = await Users.findById({ _id: res.locals.user._id }).populate(["followers", "followings"]);
-
-    res.status(200).render('dashboard', {
-        link: "dashboard",
-        photos,
-        
-    });
-    
-}
+  res.status(200).render("dashboard", {
+    link: "dashboard",
+    photos,
+    user,
+  });
+};
 
 const getAllUsers = async (req, res) => {
-
-    try {
-        const users = await Users.find({ _id: {$ne : res.locals.user._id } });
-        res.status(200).render('users', {users, link: "users"})
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-
-}
+  try {
+    const users = await Users.find({ _id: { $ne: res.locals.user._id } });
+    res.status(200).render("users", { users, link: "users" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 const getAUser = async (req, res) => {
+  try {
+    const user = await Users.findById({ _id: req.params.id });
 
-    try {
+    const inFollowers = user.followers.some((follower) => {
+      return follower.equals(res.locals.user._id);
+    });
 
-        const user = await Users.findById({_id: req.params.id});
-        const photos = await Photos.find({user: user._id});
-        res.status(200).render('user', {user, photos, link: "users"})
-        
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-
-}
+    const photos = await Photos.find({ user: user._id });
+    res
+      .status(200)
+      .render("user", { user, photos, link: "users", inFollowers });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 const follow = async (req, res) => {
+  try {
+    let user = await Users.findByIdAndUpdate(
+      { _id: req.params.id },
+      {
+        $push: { followers: res.locals.user._id },
+      },
+      { new: true }
+    );
 
-    try {
+    user = await Users.findByIdAndUpdate(
+      { _id: res.locals.user._id },
+      {
+        $push: { followings: req.params.id },
+      },
+      { new: true }
+    );
 
-        let user = await Users.findByIdAndUpdate(
-            { _id: req.params.id },
-            {
-                $push: { followers: res.locals.user._id }
-            },
-            { new: true }
-        )
-
-        user = await Users.findByIdAndUpdate(
-            { _id: res.locals.user._id },
-            {
-                $push: {followings: req.params.id }
-            },
-            { new: true }
-        )
-
-        res.status(200).json({
-            succeded: true,
-            user
-        })
-        
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-
-}
+    res.status(200).redirect(`/users/${req.params.id}`);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 const unfollow = async (req, res) => {
+  try {
+    let user = await Users.findByIdAndUpdate(
+      { _id: req.params.id },
+      {
+        $pull: { followers: res.locals.user._id },
+      },
+      { new: true }
+    );
 
-    try {
+    user = await Users.findByIdAndUpdate(
+      { _id: res.locals.user._id },
+      {
+        $pull: { followings: req.params.id },
+      },
+      { new: true }
+    );
 
-        let user = await Users.findByIdAndUpdate(
+    res.status(200).redirect(`/users/${req.params.id}`);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-            { _id: req.params.id },
-            {
-                $pull: { followers: res.locals.user._id }
-            },
-            { new: true }
-
-        )
-
-        user = await Users.findByIdAndUpdate(
-
-            { _id: res.locals.user._id },
-            {
-                $pull: {followings: req.params.id }
-            },
-            { new: true }
-
-        )
-
-        res.status(200).json({
-            succeded: true,
-            user
-        })
-        
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-
-}
-
-module.exports = {createUser, loginUser, getDashboardPage, getAllUsers, getAUser, follow, unfollow}
+module.exports = {
+  createUser,
+  loginUser,
+  getDashboardPage,
+  getAllUsers,
+  getAUser,
+  follow,
+  unfollow,
+};
